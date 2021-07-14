9f9f57f375a699fbb51641075dc6e753878ab277
obelisk0114 <obelisk0114@yahoo.com.tw>
2021-07-14 11:27:44 -0700

    Add sudoku puzzle package
    Add sudoku puzzle package

---
+++ src/Sudoku_puzzle/GeneratePuzzle.java
@@ -0,0 +1,11 @@
+package Sudoku_puzzle;
+
+/*
+ * https://en.wikipedia.org/wiki/Mathematics_of_Sudoku
+ * 
+ * http://blog.xuite.net/iading/news/23803778-%E6%95%B8%E7%8D%A8%E7%A8%8B%E5%BC%8F%E7%A2%BC+%E5%87%BA%E9%A1%8C%E8%88%87%E8%A7%A3%E9%A1%8C+C%E8%AA%9E%E8%A8%80
+ */
+
+public class GeneratePuzzle {
+
+}
